3bf87d29d373842005beb97c366be3cbef4cd0a3
lpinto <pinto120474@gmail.com>
2020-07-30 13:45:36 +0530

    added scanner code

--- src/beginJava/Hello.java
+++ src/beginJava/Hello.java
@@ -1,11 +1,21 @@
 package beginJava;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello world");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Your First Name : ");
+		String fname = sc.nextLine();
+		System.out.println("Enter Your Second Name : ");
+		String lname = sc.nextLine();
+		
+		System.out.println("Good Morning " +fname + " " +lname);
+		
 
 	}
 
